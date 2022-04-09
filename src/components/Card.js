0253f73760fce326f6ec58ac5dd47a89cf97afd6
Yash Shah <yash27052002@gmail.com>
2022-04-09 20:02:31 +0530

    smart contract

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,9 @@
 import ArticleIcon from "@mui/icons-material/Article";
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Card = () => {
-  return (
-    <Box sx={{ p: 3, bgcolor: "black" }}>
-      <ArticleIcon sx={{ color: "white" }} />
-    </Box>
-  );
+  return <Box sx={{ p: 3, bgcolor: "black" }}></Box>;
 };
 
 export default Card;
